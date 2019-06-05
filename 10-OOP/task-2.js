a5fee32b9b9e6e1bc906c9459f90422285ca8b80
Giorgi <ineveraskedtobebornmom@gmail.com>
2019-06-05 21:27:53 +0400

    feat(10-OOP): In progress tasks

---
+++ 10-OOP/task-2.js
@@ -0,0 +1,24 @@
+function CoffeeMachine(power, capacity) {
+	//...
+	this.setWaterAmount = function(amount) {
+		if (amount < 0) {
+			throw new Error("Value has to be positive.");
+		}
+		if (amount > capacity) {
+			throw new Error("You can't put more water, than " + capacity);
+		}
+		waterAmount = amount;
+	};
+    
+	this.getWaterAmount = function() {
+		return waterAmount;
+    };
+    
+    this.getPower = function() {
+        return power;
+    }
+}
+
+const userCoffeeMachine = new CoffeeMachine(16, 10);
+
+console.log(userCoffeeMachine.getPower());
